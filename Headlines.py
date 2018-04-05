9f2b521a802ad051e85845380dcad41ae2bb814b
rhys <rhyshovell@gmail.com>
2018-04-05 16:40:23 +0100

    Negative analyser added

--- Headlines.py
+++ Headlines.py
@@ -10,11 +10,11 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as SA
 hdr = {'User-Agent': 'windows:r/CryptoCurrency.single.result:v1.0' + '(by /u/Tulkas2386)'}
 url = 'https://www.reddit.com/r/CryptoCurrency/.json'
 req = requests.get(url, headers=hdr)
-json_data = json.loads(req.text)
+jsonData = json.loads(req.text)
 
-dataAll = json_data['data']['children']
+dataAll = jsonData['data']['children']
 num_posts = 0
-while len(dataAll) <= 10000:
+while len(dataAll) <= 1000:
     time.sleep(2)
     last = dataAll[-1]['data']['name']
     url = 'https://www.reddit.com/r/CryptoCurrency/.json?after='+str(last)
